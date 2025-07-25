1ba71801a1ad95e98a5db465628a9848117a4225
Soeky <seymeny527@gmail.com>
2025-07-25 16:46:05 +0200

    ads

--- hypr/screenshot.sh
+++ hypr/screenshot.sh
@@ -1,3 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-grim -g "$(slurp)" - | wl-copy
+# temporäres File für den Screenshot
+file=$(mktemp --suffix=.png)
+
+# Bereich auswählen & in die Datei speichern
+grim -g "$(slurp)" "$file"
+
+# 1) Wayland-Clipboard (wl-copy) – für lokale Wayland-Apps
+wl-copy --type image/png < "$file"
+
+# 2) XWayland-Clipboard (xclip) – damit KDE Connect/GSConnect das Bild greift
+#xclip -selection clipboard -t image/png -i "$file"
+
+#kdeconnect-cli -d d8ea2c21370a41e4a17b305f47519603 --share "$file"
+# aufräumen
+rm "$file"
